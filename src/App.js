8c1b143941f13f46e387bb46bb9dbf0006da3aa9
Rakib Raza <rakibraza159@gmail.com>
2021-06-13 09:36:11 +0600

    5 section added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dropdown from "./components/Dropdown/Dropdown";
+import { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Header />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+    </Router>
   );
 }
 
